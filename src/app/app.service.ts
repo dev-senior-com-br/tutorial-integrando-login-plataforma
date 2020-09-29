d0cb23d5d86ae53de9b579aeaa0a7a4b82ee6fb7
Wallace Reetz <wallace.reetz@gmail.com>
2020-09-29 13:48:36 -0300

    fix token service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,27 +1,25 @@
-import { Injectable } from '@angular/core'; 
-import { service, user } from '@seniorsistemas/senior-platform-data'; 
-import { SeniorApi, ENVIRONMENTS } from "@seniorsistemas/senior-core";
-import { Observable, forkJoin, from } from 'rxjs'; 
-import { concatMap } from 'rxjs/operators'; 
-import { environment } from "../environments/environment";
+import { Injectable } from '@angular/core';
+import { service, user } from '@seniorsistemas/senior-platform-data';
+import { SeniorApi, ENVIRONMENTS } from '@seniorsistemas/senior-core';
+import { Observable, forkJoin, from } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
-@Injectable() export class AppService { 
+@Injectable()
+export class AppService {
   private seniorApi: SeniorApi;
   constructor() {
     this.seniorApi = new SeniorApi();
-    if(environment.production) {
-      this.seniorApi.environment = ENVIRONMENTS.PROD
+    if (environment.production) {
+      this.seniorApi.environment = ENVIRONMENTS.PROD;
     } else {
-      this.seniorApi.environment = ENVIRONMENTS.DEV
+      this.seniorApi.environment = ENVIRONMENTS.DEV;
     }
+    user.getToken().then((token) => {
+      this.seniorApi.accessToken = token.access_token;
+    });
   }
-  getUser(): Observable<any> { 
-    return from(user.getToken())
-      .pipe(
-        concatMap(token => { 
-          this.seniorApi.accessToken = token.access_token;
-          return from(this.seniorApi.users.getUser());
-        })
-      );
-  } 
+  getUser(): Observable<any> {
+    return from(this.seniorApi.users.getUser());
+  }
 }
